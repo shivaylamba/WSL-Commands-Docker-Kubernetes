135d2462dedcec49dbc791fb2ee0828c28326141
Shivay Lamba <shivaylamba@gmail.com>
2021-02-06 23:06:32 +0530

    Create powershell.sh

---
+++ powershell.sh
@@ -0,0 +1,23 @@
+wsl -v 
+
+wsl -l -v
+
+wsl --shutdown
+ 
+wsl -l -v 
+
+wsl --help 
+
+wsl --set-default-version 2 
+
+wsl --help 
+
+wsl --set-default-version 2
+
+wsl -l -v
+
+wsl --set-version Ubuntu-20.04 2
+
+wsl -l -v
+
+wsl -l -v
