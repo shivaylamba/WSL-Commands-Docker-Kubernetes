7c29e87138c86ad9290177a5c0816607fd6ee99f
Shivay Lamba <shivaylamba@gmail.com>
2021-02-06 23:12:31 +0530

    Create ubuntu-docker-2.sh

---
+++ ubuntu-docker-2.sh
@@ -0,0 +1,20 @@
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+
+
+
+sudo service docker start 
+
+docker wait shivay 
+
+docker ps 
+
+ps -ef 
+
+sudo service docker status 
+
+docker version 
+
+docker image ls 
+
+docker pull alpine 
